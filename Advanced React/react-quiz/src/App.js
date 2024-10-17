ea3ad5459b086bba0c3a22c6bf2f79fe6820c2f7
Jadline <njerijadline@gmail.com>
2024-10-17 13:12:55 +0300

    update changes

--- Advanced React/react-quiz/src/App.js
+++ Advanced React/react-quiz/src/App.js
@@ -6,6 +6,8 @@ import Error from "./Error"
 import StartScreen from "./StartScreen"
 import Question from "./Question"
 import NextButton from "./nextbutton"
+import ProgressBar from "./progress"
+import FinishScreen from "./FinishScreen"
 
 const initialState = {
   questions : [],
@@ -14,7 +16,8 @@ const initialState = {
   status : 'loading',
   index : 0,
   answer : null,
-  points : 0
+  points : 0,
+  Highscore : 0
 }
 
 function reducer(state,action){
@@ -38,7 +41,14 @@ function reducer(state,action){
     case "nextQuestion":
       return {
         ...state,
-        index : state.index + 1
+        index : state.index + 1,
+        answer : null
+      }
+    case "finish":
+      return {
+        ...state,
+        status : 'finish',
+        Highscore : state.points  > state.Highscore ?  state.points : state.Highscore
       }
     case "newAnswer":
       const currentQuestion = state.questions.at(state.index)
@@ -48,6 +58,13 @@ function reducer(state,action){
         answer : action.payload,
         points : action.payload ===  currentQuestion.correctOption ?  state.points + currentQuestion.points : state.points
       }
+    case "restart" :
+      return {
+        ...initialState,
+        questions : state.questions,
+        status : "ready"
+        
+      }
     default : 
       throw new Error ('action is unknown')
 
@@ -56,9 +73,10 @@ function reducer(state,action){
 
 
 function App() {
-  const [{questions,status,index,answer}, dispatch] = useReducer(reducer,initialState)
+  const [{questions,status,index,answer,points,Highscore}, dispatch] = useReducer(reducer,initialState)
 
   const numQuestions = questions.length;
+  const maxPossiblePoints = questions.reduce((prev,cur)=> prev + cur.points,0)
 
   useEffect(() => {
     fetch("http://localhost:8000/questions")
@@ -75,13 +93,15 @@ function App() {
             {status === 'ready' && <StartScreen numQuestions ={numQuestions} dispatch={dispatch}/>}
             {status === "active" && 
             <>
+            <ProgressBar index={index} numQuestions={numQuestions} points={points} maxPossiblePoints={maxPossiblePoints} answer={answer}/>
               <Question 
                 question={questions[index]} 
                 dispatch={dispatch}
                 answer={answer}/>
-              <NextButton dispatch={dispatch} answer={answer}/>
+              <NextButton dispatch={dispatch} answer={answer} index={index} numQuestions={numQuestions}/>
             </>
             }
+            {status === "finish" &&  <FinishScreen points={points} maxPossiblePoints={maxPossiblePoints} index={index} numQuestions={numQuestions} Highscore={Highscore} dispatch={dispatch}/>}
            
         </main>
     </div>
---
+++ Advanced React/react-quiz/src/FinishScreen.js
@@ -0,0 +1,29 @@
+function FinishScreen({points,maxPossiblePoints,Highscore,dispatch}){
+    const percentage  = (points / maxPossiblePoints) * 100
+
+    let emoji;
+    if(percentage === 100) emoji = '🥇'
+    if(percentage >= 80 && percentage < 100) emoji = '🎉'
+    if(percentage >= 50 && percentage < 80) emoji = '😊'
+    if(percentage >= 0 && percentage < 50) emoji = '🤔'
+    if(percentage ===0) emoji="🤦‍♀️"
+
+
+    
+    return (
+        <>
+        <p className="result">
+            <span>{emoji}</span>You score <strong> {points}</strong> out of the {maxPossiblePoints}
+            
+            &nbsp; ({Math.ceil(percentage)}%)
+        </p>
+        <p className="highscore">
+            Highscore : {Highscore} points
+        </p>
+        <button className="btn btn-ui" onClick={() => dispatch({type : "restart"})}>Restart Quiz</button>
+        </>
+        
+   
+    )
+}
+export default FinishScreen
